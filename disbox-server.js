2662372f4076ac5c627e1a142ff4ff19cd7c72d8
DisboxApp <a>
2022-02-15 23:38:03 +0200

    Changed port to 8080.

--- disbox-server.js
+++ disbox-server.js
@@ -5,9 +5,8 @@ var bodyParser = require('body-parser')
 
 const disboxFile = require('./disbox-file');
 
-
-const app = express()
-const port = 3000
+const app = express();
+const port = 8080;
 
 app.use(cors())
 app.use(bodyParser.json())
