cbb5f5b3504e26780dcb08b24631e465cc8107d7
DisboxApp <a>
2022-02-16 23:17:50 +0200

    Configured for azure app services.

--- disbox-file.js
+++ disbox-file.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('./disbox.db')
+const db = new sqlite3.Database('/home/disbox.db')
 
 db.run(`
 CREATE TABLE IF NOT EXISTS files (
